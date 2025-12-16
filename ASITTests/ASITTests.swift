b96927a6ed5fa265a27b840a83aee6d6813d0325
Egor Malyshev <fidget470@gmail.com>
2025-12-16 21:42:17 +0300

    create project

---
+++ ASITTests/ASITTests.swift
@@ -0,0 +1,16 @@
+//
+//  ASITTests.swift
+//  ASITTests
+//
+//  Created by Egor Malyshev on 16.12.2025.
+//
+
+import Testing
+
+struct ASITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
