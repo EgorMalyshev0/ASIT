d07038f752da48506c1b10a8a2f3dbf8eaad3cbb
Egor Malyshev <fidget470@gmail.com>
2025-12-18 17:54:49 +0300

    add main empty view

--- ASIT/Sources/Modules/Main/MainView.swift
+++ ASIT/Sources/Modules/Main/MainView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct MainView: View {
     @State private var viewModel: ViewModel
+    @State private var isCourseAddingPresented: Bool = false
 
     init(modelContext: ModelContext) {
         let viewModel = ViewModel(modelContext: modelContext)
@@ -18,6 +19,9 @@ struct MainView: View {
 
     var body: some View {
         content
+            .sheet(isPresented: $isCourseAddingPresented) {
+                Text("Hello")
+            }
     }
 }
 
@@ -25,10 +29,37 @@ private extension MainView {
     @ViewBuilder
     var content: some View {
         if viewModel.courses.isEmpty {
-            EmptyView()
+            MainEmptyView {
+                isCourseAddingPresented = true
+            }
         } else {
             EmptyView()
         }
+    }
+}
+
+struct MainEmptyView: View {
+    let onTap: () -> Void
+
+    var body: some View {
+        VStack(spacing: 24) {
+            Text("У вас нет ни одного курса")
+                .font(.title)
+                .foregroundStyle(.primary)
+
+            if #available(iOS 26.0, *) {
+                addButton
+                    .buttonStyle(.glassProminent)
+            } else {
+                addButton
+                    .buttonStyle(.borderedProminent)
+            }
+        }
+        .padding()
+    }
+
+    private var addButton: some View {
+        Button("Добавить", action: onTap)
     }
 }
 
